6c53491adc2ea45d5089713b2d2f8cb4aa897a3e
mare hunt <mare.hunt@netgroup.ee>
2015-08-03 18:07:20 +0300

    tabletop architecture

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,10 @@
     var method;
     var noop = function () {};
     var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
     ];
     var length = methods.length;
     var console = (window.console = window.console || {});
@@ -38,64 +38,83 @@
         msViewportStyle.appendChild(
             document.createTextNode(
                 '@-ms-viewport{width:auto!important}'
+                )
             )
-        )
         document.querySelector('head').appendChild(msViewportStyle)
     }
 })();
 
+var appartmentNr;
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
     initSpreadsheet();
-
+    createPageNr();
 });
-
 
 function initSpreadsheet() {
   Tabletop.init( { 
     key: '1zY_w5nVHgh8D2GcVyV8v055gkFjTsqt13FGXMzKaqeM',
-    callback: displayData,
+    callback: displayPricelist,
     simpleSheet: true } );
-
-  function displayData(data) {
-      var i,
-      dataLength = data.length;
-
-      for (i=0; i<dataLength; i++) {
-        $('#tabledata').append(
-          $('<tr>
-            <th scope="row">' + data[i].krtnr +'</th>
-            <td>' + data[i].korrus +'</td>
-            <td>' + data[i].suurus +'</td>
-            <td>' + data[i].tube +'</td>
-            <td>' + data[i].rodu +'</td>
-            <td>' + data[i].terrass +'</td>
-            <td>' + data[i].mhind +'</td>
-            <td>' + data[i].hind +'</td>
-            <td>' + data[i].lisainfo +'</td>
-            <td>' + data[i].staatus +'</td>
-            </tr>'
-          )
-        );
-      }
-
-  }
-
-    // var tableColumns =   [
-    //     {'mDataProp': 'brewery', 'sTitle': 'Brewery', 'sClass': 'center'},
-    //     {'mDataProp': 'city', 'sTitle': 'City', 'sClass': 'center'},
-    //     {'mDataProp': 'website', 'sTitle': 'Website', 'sClass': 'center'}
-    // ];
-
-     // create the table container and object
-    // function writeTableWith(dataSource){
-    //     jqueryNoConflict('#demo1').html('<table cellpadding="0" cellspacing="0" border="0" class="display table table-bordered table-striped" id="data-table-container"></table>');
-
-    //     var oTable = jqueryNoConflict('#data-table-container').dataTable({
-    //         'aaData': dataSource,
-    //         'aoColumns': createTableColumns()
-    //     });
-    // };
 }
+
+function displayPricelist(data, tabletop) {
+    var i,
+    dataLength = data.length;
+
+    if($("#tableData").length) {
+        for (i=0; i<dataLength; i++) {
+
+            $('#tableData').append(
+                $('<tr class="clickable-row" data-url="hinnad/krt-' + data[i].krtnr + '.html">
+                    <th scope="row">' + data[i].krtnr +'</th>
+                    <td>' + data[i].korrus +'</td>
+                    <td>' + data[i].suurus +'</td>
+                    <td>' + data[i].tube +'</td>
+                    <td>' + data[i].rodu +'</td>
+                    <td>' + data[i].terrass +'</td>
+                    <td>' + data[i].mhind +'</td>
+                    <td>' + data[i].hind +'</td>
+                    <td>' + data[i].lisainfo +'</td>
+                    <td>' + data[i].staatus +'</td>
+                    </tr>'
+                )
+            );
+        }
+
+        $(".clickable-row").click(function() {
+            window.document.location = $(this).data('url');
+        }); 
+    }
+    else if ($("#appartmentDetailTable").length) {
+        var apartmentNr = getApartmentNr();
+        var dataApartment = data[apartmentNr-1];
+
+        $('#appartmentDetailTable').append(
+            $('<tr class="clickable-row" data-url="hinnad/krt-' + dataApartment.krtnr + '.html">
+                <th scope="row">' + dataApartment.hind +'</th>
+                <td>' + dataApartment.krtnr +'</td>
+                </tr>'
+            )
+        );
+    }
+}
+
+function createPageNr() {
+    var apartmentNr = getApartmentNr();
+    $('#appartmentDetailHead').append(
+        $('<h1> Krt nr on ' + apartmentNr + '</h1>'
+        )
+    );
+}
+
+function getApartmentNr() {
+    var href = $(window.document.location).attr("href");
+    var lastSegment = href.split('/').pop();
+    var removeHtml = lastSegment.split('.').shift();
+    return removeHtml.split('-').pop();
+}
+
 
 
